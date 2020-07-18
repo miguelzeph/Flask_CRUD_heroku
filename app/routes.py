efa5c0cd618b9fd943decc4653f6fbce20b7b5f4
miguel <miguel.junior.mat@hotmail.com>
2020-07-18 18:24:12 -0300

    Template Base CRUD OK

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,13 @@
 from app import app
+from flask import render_template, request, session
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
+
+@app.route('/enviar', methods = ['POST','GET'])
+def enviar():
+    if request.method == 'POST':
+        print('oi')
+        print(request.form["name"])
+    return render_template('index.html')
