81d8d1057e1563c12a8360ecabe2d198932b3f00
miguel <miguel.junior.mat@hotmail.com>
2020-07-20 16:00:38 -0300

    deploy

--- run.py
+++ run.py
@@ -8,4 +8,5 @@ if not os.path.exists('data.sqlite'):
 from app import routes
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
