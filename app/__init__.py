dfab3560502789865705a464b845d7efe599f1c0
miguel <miguel.junior.mat@hotmail.com>
2020-07-20 16:23:54 -0300

    environ heroku

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SECRET_KEY'] = 'mysecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
 
